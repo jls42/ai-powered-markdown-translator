399a80e24a3a645d26328b11e51b2fab95123a21
Julien LE SAUX <le.saux.julien@gmail.com>
2024-01-14 15:26:38 +0100

    init

---
+++ README.md
@@ -0,0 +1,86 @@
+Voici le contenu du README en Markdown pour votre script de traduction :
+
+# Traducteur de Markdown avec OpenAI
+
+Ce projet est un script Python qui utilise l'API OpenAI pour traduire des fichiers Markdown d'une langue source vers une langue cible.
+Plus d'information sur ![Traduction IA jls42.org](https://jls42.org/posts/ia/automatisation-traduction-ia/).  
+
+## Prérequis
+
+Pour utiliser ce script, vous aurez besoin de :
+
+- Python 3.6 ou supérieur
+- Un compte OpenAI avec une clé API
+
+## Installation
+
+1. Clonez ce dépôt sur votre machine locale.
+2. Installez les dépendances nécessaires en utilisant pip :
+
+```bash
+pip install -r requirements.txt
+```
+
+## Utilisation
+
+Pour utiliser ce script, vous devez d'abord définir votre clé API OpenAI comme variable d'environnement :
+
+```bash
+export OPENAI_API_KEY='votre-clé-api'
+```
+
+
+En suite, vous pouvez exécuter le script en utilisant la commande suivante :
+
+```bash
+python translate.py --source_dir 'chemin/vers/votre/répertoire/source' --target_dir 'chemin/vers/votre/répertoire/cible'
+```
+
+Vous pouvez également spécifier le modèle à utiliser, la langue source et la langue cible :
+
+```bash
+python translate.py --source_dir 'chemin/vers/votre/répertoire/source' --target_dir 'chemin/vers/votre/répertoire/cible' --model 'gpt-4-1106-preview' --source_lang 'fr' --target_lang 'en'
+```
+
+## Exemples d'usage
+
+```bash
+################################################
+# Demande de traduction à l'IA vers l'espagnol #
+################################################
+jls42@Boo:~/blog/jls42$ python3 translate.py --source_dir content/ --target_dir content/traductions_es --target_lang es
+Traitement du fichier : content/posts/ia/stable-difusion-aws-ec2.md
+Traduction terminée en 33.19 secondes.
+Fichier 'stable-difusion-aws-ec2.md' traité.
+Traitement du fichier : content/posts/ia/poc-openai-api-gpt4.md
+Traduction terminée en 25.24 secondes.
+Fichier 'poc-openai-api-gpt4.md' traité.
+Traitement du fichier : content/posts/ia/poc-mistral-ai-mixtral.md
+Traduction terminée en 58.78 secondes.
+Fichier 'poc-mistral-ai-mixtral.md' traité.
+Traitement du fichier : content/posts/raspberry-pi/installation-de-kubernetes-sur-raspberry-pi-via-ansible.md
+Traduction terminée en 17.64 secondes.
+Fichier 'installation-de-kubernetes-sur-raspberry-pi-via-ansible.md' traité.
+Traitement du fichier : content/posts/raspberry-pi/installation-de-docker-sur-raspberry-pi-via-ansible.md
+Traduction terminée en 19.60 secondes.
+Fichier 'installation-de-docker-sur-raspberry-pi-via-ansible.md' traité.
+Traitement du fichier : content/posts/raspberry-pi/initialisation-auto-de-raspbian-sur-raspberry-pi.md
+Traduction terminée en 37.12 secondes.
+Fichier 'initialisation-auto-de-raspbian-sur-raspberry-pi.md' traité.
+Traitement du fichier : content/posts/blog/nouveau-theme-logo.md
+Traduction terminée en 18.91 secondes.
+Fichier 'nouveau-theme-logo.md' traité.
+Traitement du fichier : content/posts/infrastructure/infrastruture-as-code-serverless-ha-jls42-org.md
+Traduction terminée en 30.73 secondes.
+Fichier 'infrastruture-as-code-serverless-ha-jls42-org.md' traité.
+Traitement du fichier : content/mentions/mentions-legales.md
+Traduction terminée en 13.14 secondes.
+Fichier 'mentions-legales.md' traité.
+Traitement du fichier : content/about/a-propos-du-blog-jls42.md
+Traduction terminée en 11.24 secondes.
+Fichier 'a-propos-du-blog-jls42.md' traité.
+```
+
+## Licence
+
+Ce projet est sous licence GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007. Voir le fichier [LICENSE](LICENSE) pour plus de détails.
---
+++ translate.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import time
+from openai import OpenAI
+import re
+
+# Initialisation de la configuration avec les valeurs par défaut
+DEFAULT_API_KEY = 'votre-clé-api-par-défaut'
+DEFAULT_MODEL = "gpt-4-1106-preview"
+DEFAULT_SOURCE_LANG = 'fr'
+DEFAULT_TARGET_LANG = 'en'
+DEFAULT_SOURCE_DIR = 'content/posts'
+DEFAULT_TARGET_DIR = 'traductions_en'
+
+MODEL_TOKEN_LIMITS = {
+    "gpt-4-1106-preview": 4096,
+    "gpt-4-vision-preview": 4096,
+    "gpt-4": 8192,
+    "gpt-4-32k": 32768,
+    "gpt-4-0613": 8192,
+    "gpt-4-32k-0613": 32768
+}
+
+# Fonction de traduction
+def translate_with_openai(text, client, args):
+    """
+    Traduit le texte donné du langage source au langage cible en utilisant l'API OpenAI.
+    
+    Args:
+        text (str) : Le texte à traduire.
+        client : L'objet client OpenAI.
+        args : Les arguments contenant les informations sur le langage source, le langage cible et le modèle.
+        
+    Returns:
+        str : Le texte traduit.
+    """
+    # Détecter et stocker les blocs de code
+    code_blocks = re.findall(r'(```.*?```)', text, flags=re.DOTALL)
+    placeholders = [f"#CODEBLOCK{index}#" for index, _ in enumerate(code_blocks)]
+    
+    # Remplacer les blocs de code par des placeholders
+    for placeholder, code_block in zip(placeholders, code_blocks):
+        text = text.replace(code_block, placeholder)
+    
+    # Création du message pour l'API
+    messages = [
+        {"role": "system", "content": f"Translate the following text from {args.source_lang} to {args.target_lang}, ensuring that elements such as URLs, image paths, and code blocks (delimited by ```) are not translated. Leave these elements unchanged."},
+        {"role": "user", "content": text}
+    ]
+    
+    # Envoi de la demande de traduction
+    response = client.chat.completions.create(
+        model=args.model,
+        messages=messages
+    )
+    
+    # Obtenir le texte traduit et remplacer les placeholders par les blocs de code originaux
+    translated_text = response.choices[0].message.content.strip()
+    for placeholder, code_block in zip(placeholders, code_blocks):
+        translated_text = translated_text.replace(placeholder, code_block)
+
+    return translated_text
+
+def add_translation_note(client, args):
+    """
+    Ajoute une note de traduction à un document.
+
+    Args:
+        client : Le client de traduction.
+        args : Arguments supplémentaires.
+
+    Returns:
+        La note de traduction formatée.
+    """
+    # Note de traduction en français
+    translation_note_fr = "Ce document a été traduit de la version française du blog par le modèle "
+    # Traduire la note en langue cible
+    translated_note = translate_with_openai(translation_note_fr + args.model, client, args)
+    # Formatage de la note de traduction
+    return f"\n\n**{translated_note}**\n\n"
+
+# Traitement des fichiers Markdown
+def translate_markdown_file(file_path, output_path, client, args):
+    """
+    Traduit le contenu d'un fichier markdown en utilisant l'API de traduction OpenAI et écrit le contenu traduit dans un nouveau fichier.
+
+    Args:
+        file_path (str): Chemin vers le fichier markdown d'entrée.
+        output_path (str): Chemin vers le fichier de sortie où le contenu traduit sera écrit.
+        client: Client de traduction OpenAI.
+        args: Arguments supplémentaires pour le processus de traduction.
+
+    Returns:
+        None
+    """
+    print(f"Traitement du fichier : {file_path}")
+    start_time = time.time()
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    translated_content = translate_with_openai(content, client, args)
+    
+    # Ajouter la note de traduction à la fin du contenu traduit
+    translation_note = add_translation_note(client, args)
+    translated_content_with_note = translated_content + translation_note
+
+    with open(output_path, 'w', encoding='utf-8') as f:
+        f.write(translated_content_with_note)
+
+    end_time = time.time()
+    print(f"Traduction terminée en {end_time - start_time:.2f} secondes.")
+
+def translate_directory(input_dir, output_dir, client, args):
+    """
+    Traduit tous les fichiers markdown dans le répertoire d'entrée et ses sous-répertoires.
+
+    Args:
+        input_dir (str): Chemin vers le répertoire d'entrée.
+        output_dir (str): Chemin vers le répertoire de sortie.
+        client: Objet client de traduction.
+        args: Arguments supplémentaires pour la traduction.
+
+    Returns:
+        None
+    """
+    for root, dirs, files in os.walk(input_dir, topdown=True):
+        # Exclure les dossiers qui commencent par "traductions_"
+        dirs[:] = [d for d in dirs if not d.startswith("traductions_")]
+
+        for file in files:
+            if file.endswith('.md'):
+                file_path = os.path.join(root, file)
+                base, _ = os.path.splitext(file)
+                # Ajouter le nom du modèle utilisé dans le nom du fichier de sortie
+                output_file = f"{base}-{args.model}-{args.target_lang}.md"
+                relative_path = os.path.relpath(root, input_dir)
+                output_path = os.path.join(output_dir, relative_path, output_file)
+
+                os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+                if not os.path.exists(output_path):
+                    translate_markdown_file(file_path, output_path, client, args)
+                    print(f"Fichier '{file}' traité.")
+
+
+def main():
+    """
+    Fonction principale pour traduire les fichiers Markdown.
+
+    Args:
+        --source_dir (str): Répertoire source contenant les fichiers Markdown.
+        --target_dir (str): Répertoire cible pour sauvegarder les traductions.
+        --model (str): Modèle GPT à utiliser.
+        --target_lang (str): Langue cible pour la traduction.
+        --source_lang (str): Langue source pour la traduction.
+    """
+    parser = argparse.ArgumentParser(description="Traduit les fichiers Markdown.")
+    parser.add_argument('--source_dir', type=str, default=DEFAULT_SOURCE_DIR, help='Répertoire source contenant les fichiers Markdown')
+    parser.add_argument('--target_dir', type=str, default=DEFAULT_TARGET_DIR, help='Répertoire cible pour sauvegarder les traductions')
+    parser.add_argument('--model', type=str, default=DEFAULT_MODEL, help='Modèle GPT à utiliser')
+    parser.add_argument('--target_lang', type=str, default=DEFAULT_TARGET_LANG, help='Langue cible pour la traduction')
+    parser.add_argument('--source_lang', type=str, default=DEFAULT_SOURCE_LANG, help='Langue source pour la traduction')
+
+    args = parser.parse_args()
+
+    openai_api_key = os.getenv('OPENAI_API_KEY', DEFAULT_API_KEY)
+    with OpenAI(api_key=openai_api_key) as client:
+        translate_directory(args.source_dir, args.target_dir, client, args)
+
+if __name__ == "__main__":
+    main()
